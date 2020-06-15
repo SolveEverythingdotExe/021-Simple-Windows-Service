41d7d9067f2a84b51fc8da4f6bb7cbb37a8d981e
SolveEverything.exe <solveeverything.exe@gmail.com>
2020-06-15 15:48:24 +0800

    Initial commit

---
+++ FirstWindowsServiceProject/FirstWindowsServiceProject/FirstWindowsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWindowsServiceProject
+{
+    public partial class FirstWindowsService : ServiceBase
+    {
+        public FirstWindowsService()
+        {
+            InitializeComponent();
+
+            //now lets set the properties of the eventlog
+
+            //this will be the subcategory of the log on Event Viewer > Windows Log
+            eventLog1.Log = "Application";
+
+            //this will be the name that will appear on "Source" column in Event Viewer
+            eventLog1.Source = "FirstWindowsService";
+        }
+
+        //now lets write on the log once the service is started
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("The user starts the service.", EventLogEntryType.Information);
+        }
+
+        //also once it is stopped
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("The user stops the service.", EventLogEntryType.Information);
+        }
+
+        //it's now done
+        //now lets add an Installer on the designer
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Simple Windows Service
+
+This project shows how to create a simple windows service that writes log entry on System Event Viewer whenever the service is started or stopped.
